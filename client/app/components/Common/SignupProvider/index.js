85e188bfa2f6cf52f7445875bc94aa7a58f83014
ucdc05 <109817657+ucdc05@users.noreply.github.com>
2023-01-11 09:19:31 -0800

    Update index.js

--- client/app/components/Common/SignupProvider/index.js
+++ client/app/components/Common/SignupProvider/index.js
@@ -11,7 +11,15 @@ import { BASE_API_URL } from '../../../constants';
 
 const SignupProvider = () => {
   return (
+    <div className='signup-provider'>
+      <a href={`${BASE_API_URL}/auth/google`} className='mb-2 google-btn'>
+        
+      </a>
 
+      <a href={`${BASE_API_URL}/auth/facebook`} className='facebook-btn'>
+      
+      </a>
+    </div>
   );
 };
 
